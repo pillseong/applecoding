c07d522bd3d90ead23861948aef7aaa04fe7d733
pillseong <vlfwen123@naver.com>
2024-02-27 06:49:02 +0900

    apple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
+function App(){
+  const [text,settext]=useState("남자코트추천");
+  const [text2,settext2]=useState("강남 우동맛집");
+  const [text3,settext3]=useState("파이썬독학");
 
-function App() {
-  return (
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-box">
+        <h4>react blog</h4>
+      </div>
+    <div className="list">
+    <h4>{text}</h4>
+    <p>2월 17일 발일</p>
     </div>
-  );
+    <div className="list">
+    <h4>{text2}</h4>
+    <p>2월 17일 발일</p>
+    </div>
+    <div className="list">
+    <h4>{text3}</h4>
+    <p>2월 17일 발일</p>
+    </div>
+    </div>
+  )
 }
-
 export default App;
